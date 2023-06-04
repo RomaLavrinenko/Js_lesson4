4b98184c0313c9555885be8cdb4b7baa6a652038
RomaLavrinenko <73121041+RomaLavrinenko@users.noreply.github.com>
2023-06-04 20:50:17 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,96 @@
+//Завдання 1
+// let password = '1234';
+// let text = prompt("Введіть пароль");
+// let i = 4;
+// while(i>=1 && password != text){
+//     text = prompt(`Пароль не правильний. У вас залишилось ${i} спроб`);
+//     i--;
+   
+// }
+// if (password == text){
+//     alert("Запрошуємо на сайт!");
+// }
+// else if(i == 0){
+//     alert("Будь ласка спробуйте пізніше!");
+// }
+
+
+
+
+//Завдання 2.1
+// let fact = +prompt("Введіть факторіал ")
+// let result = 1, i = 1;
+// while(i<=fact){
+//     result*=i;
+//     i++;
+// }
+// console.log(result);
+
+//Завдання 2.2
+// let i = 1000;
+// while(i<=9999){
+//     i+=3;
+//     console.log(i);
+// }
+
+//Завдання 2.3
+// let result = 1;
+// let i = 1;
+// while(i<=55){
+//     console.log(result);
+//     result+=2;
+//     i++;
+    
+// }
+
+//Завдання 2.4
+// let i = 90;
+// while(i>=0) {
+//     console.log(i);
+//     i-=5;
+// }
+
+//Завдання 2.5
+// let i = 1;
+// let result =2;
+// while(i<=20){
+//     console.log(result);
+//     result*=2;
+//     i++;
+// }
+
+//Завдання 2.6
+// let a = 2;
+// while( a <= 10000) {
+//     console.log(a);
+//     a = 2*a-1;
+// }
+
+//Завдання 2.7
+// let a = -166;
+// while( a <= 100) {
+//     console.log(a);
+//     a = 2*a+200;
+// }
+
+//Завдання 2.8
+// let a = +prompt("Введіть число");
+// let b = +prompt("Введіть степінь");
+// let result = 1;
+// let i = 1;
+// while(i<=b){
+//     result *=a;
+//     i++
+// }
+// if(b<0){
+//     console.log(parseFloat( 1 / result));
+// }
+// else if(b==0){
+//     console.log(a**0)
+// }
+// else if(a>0 && b>0){
+//     console.log(result);
+// }
+// else{
+//     console.log("Error");
+// }
